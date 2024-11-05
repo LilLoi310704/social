7fc03017a9b042a002ad6dac9add74274bb2c3ca
LilLoi310704 <loinguyen.310704@gmail.com>
2024-11-05 17:11:50 +0700

    LilLoi

---
+++ SocialProject/app/src/main/java/com/vogella/android/socialproject/layouts/test.kt
@@ -0,0 +1,9 @@
+package com.vogella.android.socialproject.layouts
+
+import androidx.compose.runtime.Composable
+import com.example.social.layouts.GetNenHinhDaiDien
+
+@Composable
+fun test(){
+    GetNenHinhDaiDien()
+}
